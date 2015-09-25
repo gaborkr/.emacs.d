877cdd6f3358cafba5ee483ec3ca61d58e4e8b02
Gabor Kulcsar <gkulcsar@gmail.com>
2015-09-25 16:30:17 +0100

    Enabled ido-vertical-mode

--- modules/init-ido.el
+++ modules/init-ido.el
@@ -4,9 +4,9 @@
 (require 'ido-ubiquitous)
 (ido-ubiquitous-mode)
 
-;(require 'ido-vertical-mode)
+(require 'ido-vertical-mode)
 (ido-mode 1)
-;(ido-vertical-mode 1)
+(ido-vertical-mode 1)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
 (provide 'init-ido)
